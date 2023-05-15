d5aca2ef7a50137fe566c3b25eb8a771961f2467
jpalafox12 <53842278+jpalafox12@users.noreply.github.com>
2023-05-15 10:07:05 -0600

    AGREGADO CITAS FRAGMENT POR ID DE USUARIO INICIADO SESION

--- app/src/main/java/com/example/mrcantt2/AgendarMedicaFragment.kt
+++ app/src/main/java/com/example/mrcantt2/AgendarMedicaFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
+import com.example.mrcantt2.databinding.FragmentAgendarMedicaBinding
 import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,21 +20,23 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class AgendarMedicaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
+    private lateinit var binding: FragmentAgendarMedicaBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val datePicker = view?.findViewById<DatePicker>(R.id.date_picker_cita_vacunacion)
-        val calendar = Calendar.getInstance()
-        datePicker?.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(
-            Calendar.DAY_OF_MONTH), null)
-
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agendar_medica, container, false)
-
+        binding = FragmentAgendarMedicaBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val tipoCita = arguments?.getString("tipo_cita")
+
+        // Usa el valor de tipoCita para configurar la interfaz de usuario
+    }
 }
